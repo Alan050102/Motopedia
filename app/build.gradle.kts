2d118bf9c6632a651283eb3d4ef9a41d9d0ef2ce
Alan050102 <alan.nuno4804@alumnos.udg.mx>
2024-04-25 09:55:23 -0600

    implementation of the activities of creating user and recovering password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -9,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.motopedia"
-        minSdk = 16
+        minSdk = 22
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -46,6 +47,13 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/motopedia/MainActivity.kt
+++ app/src/main/java/com/example/motopedia/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.motopedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+enum class  ProviderType {
+    BASIC,
+    GOOGLE
+}
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        auth = Firebase.auth
+        var login = findViewById<Button>(R.id.login)
+        var crearcuenta = findViewById<Button>(R.id.crear)
+        var recuperarcontra = findViewById<Button>(R.id.recupera)
+        var loginGoogle = findViewById<Button>(R.id.autgoogle)
+        var email = findViewById<EditText>(R.id.email_Text)
+        var password = findViewById<EditText>(R.id.password_Text)
+
+
+        login.setOnClickListener {
+            if (email.text.toString()!= "" && password.text.toString()!= "")
+            {
+                auth.signInWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener{
+                        task ->
+
+                    if(task.isSuccessful)
+                    {
+                        startActivity(Intent(this,MenuActivity::class.java).putExtra("saludo", "Menu principal"))
+                        finish()
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"Error: "+ task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Ingresar correo y password", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
+        crearcuenta.setOnClickListener {
+            startActivity(Intent(this,AgregarUsuarios::class.java).putExtra("saludo", "Crear Cuenta"))
+        }
+        recuperarcontra.setOnClickListener {
+            startActivity(Intent(this,RecuperarContra::class.java).putExtra("saludo", "Recuperar password"))
+        }
+
+
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser == null)
+        {
+            Toast.makeText(this,"No hay usuarios Autenticados", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            startActivity(Intent(this, MenuActivity::class.java))
+            finish()
+        }
     }
 }
